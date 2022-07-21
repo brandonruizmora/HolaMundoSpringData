c8bb318a6d2aa36a819b03b89ff3e0999655bc5b
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-20 21:29:14 -0500

    Added Person persistance domain set values on controller and used in view index

---
+++ src/main/java/mx/com/gm/HolaMundoSpringData/domain/Person.java
@@ -0,0 +1,13 @@
+package mx.com.gm.HolaMundoSpringData.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+
+}
--- src/main/java/mx/com/gm/HolaMundoSpringData/web/BeginController.java
+++ src/main/java/mx/com/gm/HolaMundoSpringData/web/BeginController.java
@@ -1,13 +1,23 @@
 package mx.com.gm.HolaMundoSpringData.web;
 
+import mx.com.gm.HolaMundoSpringData.domain.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BeginController {
 
     @GetMapping("/")
-    public String start() {
+    public String start(Model model) {
+
+        Person person1 = new Person();
+        person1.setName("Brandon");
+        person1.setLastName("Ruiz");
+        person1.setEmail("bruiz@email.com");
+        person1.setPhone("1234567890");
+
+        model.addAttribute("person", person1);
         return "index";
     }
 }
